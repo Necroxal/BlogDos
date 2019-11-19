b6aca89481d4186d2f58a20821b48004751ddda4
German Rangel <gnrg_74@hotmail.com>
2019-11-19 11:54:41 -0600

    Se crearon los templates, se configuraron las rutas y se creo una aplicacion para manejo de cntenidos estaticos

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+def home(request):
+    return render(request,'core/home.html')
+def portafolio(request):
+     return render("core/portafolio.html")
+def contacto(request):
+     return render("core/contacto.html")
+def cv(request):
+     return render("core/cv.html")
+ 
 # Create your views here.
